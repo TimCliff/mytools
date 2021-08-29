bf0c7e01da31318aff98c7b86432c83a749a1673
TimCliff <dryseed@hotmail.com>
2021-08-29 13:33:16 -0500

    Update email_test.bash

--- email_test.bash
+++ email_test.bash
@@ -4,7 +4,7 @@ email_to="TODO@umn.edu"
 email_from="TODO@hotmail.com"
 email_pw="TODO"
 
-sendemail -f $email_from -t $email_to -u subject -m "Witness Test EU" -s smtp.live.com -o tls=yes -xu $email_from -xp $email_pw
+sendemail -f $email_from -t $email_to -u subject -m "Witness Test" -s smtp.live.com -o tls=yes -xu $email_from -xp $email_pw
 
 echo "done"
 
