0ef2124203cda38fa68932ca76a4b971502a24bf
TimCliff <dryseed@hotmail.com>
2021-08-29 14:15:39 -0500

    Update cli_wallet2.bash

--- cli_wallet2.bash
+++ cli_wallet2.bash
@@ -3,4 +3,4 @@
 api_url="wss://anyx.io"
 
 cd /home/witness/
-exec /usr/bin/cli_wallet -s $api_url -d --rpc-endpoint 127.0.0.1:8092
+exec /usr/bin/cli_wallet -s $api_url -d --rpc-http-endpoint 127.0.0.1:8092 --rpc-http-allowip 127.0.0.1
