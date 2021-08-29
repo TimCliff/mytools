299b4f19c60a4f95b5436f5a5e8583a8857e097b
TimCliff <dryseed@hotmail.com>
2021-08-29 13:39:31 -0500

    Update cli_wallet2.bash

--- cli_wallet2.bash
+++ cli_wallet2.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+api_url="wss://anyx.io"
+
 cd /home/witness/
-exec /home/witness/steem/programs/cli_wallet/cli_wallet -s wss://rpc.steemliberator.com -d --rpc-endpoint 127.0.0.1:8092
+exec /home/witness/steem/programs/cli_wallet/cli_wallet -s $api_url -d --rpc-endpoint 127.0.0.1:8092
