69487029f7660693d9f9c5edb21a5a7cf9e45cfe
TimCliff <dryseed@hotmail.com>
2022-10-09 15:33:29 -0500

    Update cli_wallet2.bash

--- cli_wallet2.bash
+++ cli_wallet2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-api_url="wss://anyx.io"
+api_url="ws://hiveseed-fin.privex.io:8090"
 
 cd /home/witness/
 exec /usr/bin/cli_wallet -s $api_url -d --rpc-http-endpoint 127.0.0.1:8092 --rpc-http-allowip 127.0.0.1
